68cd630888d61e110c763024eb3f11e6ea42e937
checho rios <chechorios2008@gmail.com>
2023-03-24 00:21:43 -0500

    Creación de plantillas HTML para su renderización

--- ProyectoWeb/ProyectoWeb/urls.py
+++ ProyectoWeb/ProyectoWeb/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #No se relaciona cada URL, sino cada urls.py de las <> app's
+    path('', include('ProyectoWebApp.urls')),
+
 ]
