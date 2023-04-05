d2dddb8e74c169095db093703650b22399a79518
checho rios <chechorios2008@gmail.com>
2023-04-05 11:18:18 -0500

    Creación de modulo para la gestión de pedidos

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 from servicios.models import Servicio
 
 from carro.carro import Carro
+
+
 
 # Create your views here.
 
 def home(request):
 
     carro = Carro(request)
-
     return render(request, "ProyectoWebApp/home.html")
